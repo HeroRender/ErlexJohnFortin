10f594b15296211befda56c5a2abb60e223362ef
HeroRender <erlexpe@gmail.com>
2019-08-21 12:44:35 +0800

    added category on services, group individual properties into 1 object

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,27 @@ function objProject(Container, Source) {
     this.LoadData = function (URL) {
         $.get(URL, function (DATA) {
             thisObject.Propeties = DATA;
-            thisObject.ProjectName = DATA.ProjectName;
-            thisObject.Link = DATA.Link;
-            thisObject.Description = DATA.Description
-            thisObject.Platform = DATA.Platform
-            thisObject.ScreenShots = DATA.ScreenShots;
-
             // Render the object
             thisObject.Render();
         });
     }
 
     this.Render = function () {
+        let Prop = this.Propeties;
+        console.log(Prop.ProjectName)
         $('#' + this.Container)
             .html('')
             .append($('<h1>')
-                .text(this.ProjectName || '<Unnamed>'))
+                .text(Prop.ProjectName || '<Unnamed>'))
             .append($('<p>')
-                .text((this.Description || '<No Info>') + (this.Link ? ' For more information please click ' : ''))
+                .text((Prop.Description || '<No Info>') + (Prop.Link ? ' For more information please click ' : ''))
                 .append($('<a>')
-                    .attr('href', this.Link || '#')
-                    .text(this.Link ? 'here.' : '')))
-            .append($('<p>').text(this.Platform) || '<No Info>')
+                    .attr('href', Prop.Link || '#')
+                    .text(Prop.Link ? 'here.' : '')))
+            .append($('<p>').text(Prop.Platform) || '<No Info>')
             .append($('<div>').attr('id', divScreenShots))
 
-        CreateScreenShots(this.ScreenShots);
+        CreateScreenShots(Prop.ScreenShots);
     }
 
     // Abstracted Methods/functions
@@ -46,12 +42,7 @@ function objProject(Container, Source) {
     // Properties
     this.Propeties = {};
     this.Container = Container;
-    this.ProjectName;
-    this.Link;
-    this.Description;
-    this.Platform;
-    this.ScreenShots;
-
+    
     // Abstracted Property
     let thisObject = this;
     let divScreenShots = this.Container + '_ScreenShots';
@@ -75,4 +66,4 @@ $(document).ready(function () {
     let objWQP = new objProject('divProjWQP', 'services/wqp.json');
     let objGSQ = new objProject('divProGSQ', 'services/gsq.json');
     let objCOE = new objProject('divProjCOE', 'services/coe.json');
-})
+});
