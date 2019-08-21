e91abfd3aa04b92c2d55a338d664f1d61279f8be
HeroRender <erlexpe@gmail.com>
2019-08-21 12:26:13 +0800

    added rendering of game projects

--- scripts/index.js
+++ scripts/index.js
@@ -64,14 +64,15 @@ function objProject(Container, Source) {
 }
 
 $(document).ready(function () {
+    // Corporate
     let objSync = new objProject('divProjSync', 'services/sync.json');
-
     let objRTAV = new objProject('divProjRTAV', 'services/rtav.json')
-
     let objBridge = new objProject('divProjBridge', 'services/bridge.json');
-
     let objGPS = new objProject('divProjGPS', 'services/gps.json');
-
     let objJAC = new objProject('divProjJAC', 'services/jac.json');
 
+    // Games
+    let objWQP = new objProject('divProjWQP', 'services/wqp.json');
+    let objGSQ = new objProject('divProGSQ', 'services/gsq.json');
+    let objCOE = new objProject('divProjCOE', 'services/coe.json');
 })
